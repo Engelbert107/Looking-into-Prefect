77df445b836f384defdc106bd7eceeec7f83c5f9
Engelbert Tchinde <etchinde@gmail.com>
2024-11-24 23:35:43 -0500

    added new file 3

---
+++ pause_and_wait_3.py
@@ -0,0 +1,26 @@
+from prefect import flow, pause_flow_run
+from prefect.logging import get_run_logger
+from prefect.input import RunInput
+
+class UserInput(RunInput):
+    name: str
+    age: int
+
+@flow
+async def greet_user():
+    logger = get_run_logger()
+
+    user_input = await pause_flow_run(
+        wait_for_input=UserInput.with_initial_data(name="anonymous")
+    )
+    
+    # When a user sees the form for this input, the name field contains "anonymous" as the default.
+
+    if user_input.name == "anonymous":
+        logger.info("Hello, stranger!")
+    else:
+        logger.info(f"Hello, {user_input.name}!")
+        
+
+if __name__=="__main__":
+    greet_user()
