438d9c9900d9f34481fef4736716c9edaa6debaf
Engelbert Tchinde <etchinde@gmail.com>
2024-11-24 23:36:00 -0500

    added new file 4

---
+++ pause_and_wait_4.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from prefect import flow
+from prefect.flow_runs import pause_flow_run
+from prefect.logging import get_run_logger
+from prefect.input import RunInput
+
+# Provide a dynamic, Markdown description that appears in the Prefect UI when the flow run pauses
+
+
+class UserInput(RunInput):
+    name: str
+    age: int
+
+
+@flow
+async def greet_user():
+    logger = get_run_logger()
+    current_date = datetime.now().strftime("%B %d, %Y")
+
+    description_md = f"""
+**Welcome to the User Greeting Flow!**
+Today's Date: {current_date}
+
+Please enter your details below:
+- **Name**: What should we call you?
+- **Age**: Just a number, nothing more.
+"""
+
+    user_input = await pause_flow_run(
+        wait_for_input=UserInput.with_initial_data(
+            description=description_md, name="anonymous"
+        )
+    )
+
+    if user_input.name == "anonymous":
+        logger.info("Hello, stranger!")
+    else:
+        logger.info(f"Hello, {user_input.name}!")
+
+
+if __name__=="__main__":
+    greet_user()
