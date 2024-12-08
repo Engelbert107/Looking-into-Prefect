ba980f2036ef238b27a7009455b9f35847cb3cff
Engelbert Tchinde <etchinde@gmail.com>
2024-12-08 02:05:57 -0500

    added new file

--- custom_dockerfile.py
+++ custom_dockerfile.py
@@ -5,17 +5,22 @@ from prefect.docker import DockerImage
 @flow(log_prints=True)
 def buy():
     print("Buying securities")
-
+    
+    
 
 if __name__ == "__main__":
     buy.deploy(
         name="my-custom-dockerfile-deployment",
-        work_pool_name="my-docker-pool",
+        work_pool_name="my-docker-pool",  # Ensure this pool exists
         image=DockerImage(
-            name="my_image",
-            tag="deploy-guide",
-            dockerfile="Dockerfile"
-    ),
-    push=False
-)
+            name="my_image",  # Image name
+            tag="deploy-guide",           # Image tag
+            dockerfile="Dockerfile",       # Dockerfile path 
+            build=False                   # Disable building the image
+        ),
+        push=True  # Set to True if you want to push to a registry
+    )
+    
+
+
 
