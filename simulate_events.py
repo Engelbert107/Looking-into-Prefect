d83e55111682939a51f7918be6d92b32921dfbc9
Engelbert Tchinde <etchinde@gmail.com>
2024-12-11 15:14:01 -0500

    update with comment

--- simulate_events.py
+++ simulate_events.py
@@ -1,6 +1,11 @@
 import time
 from prefect.events import emit_event
 
+
+# user_id_1 creates and then completes an order, triggering a run of our deployment.
+# user_id_2 creates an order, but no completed event is emitted so no deployment is triggered.
+
+
 user_id_1, user_id_2 = "123", "456"
 for event_name, user_id in [
     ("order.created", user_id_1),
